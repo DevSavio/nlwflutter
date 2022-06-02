c8a64d7f6777d5bdf9dfc2b9d59c864d6d40d91e
DevSavio <develo_savio@gmail.com>
2022-06-02 11:53:58 -0300

    Arquivo de configuração de teste

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:nlwflutter/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const AppWidget());
+    await tester.pumpWidget(const AppFirebase());
     TestWidgetsFlutterBinding.ensureInitialized();
 
     // Verify that our counter starts at 0.
