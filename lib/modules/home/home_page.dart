4ad81d0bf4e6b54973ef232b3fddeee57b2f79df
DevSavio <develo_savio@gmail.com>
2022-06-02 11:24:19 -0300

    Criado a tela de HomePage

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,15 +1,109 @@
 import 'package:flutter/material.dart';
+import 'package:nlwflutter/modules/home/home_controller.dart';
+import 'package:nlwflutter/shared/themes/app_text_styles.dart';
 import 'package:nlwflutter/shared/themes/appcolors.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final homeController = HomeController();
+  final pages = [
+    Container(
+      color: Colors.red,
+    ),
+    Container(
+      color: Colors.blueAccent,
+    ),
+  ];
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: AppColors.primary,
-        title: Text("HomePage"),
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(152),
+        child: Container(
+          height: 152,
+          color: AppColors.primary,
+          child: Center(
+            child: ListTile(
+              title: Text.rich(
+                TextSpan(
+                    text: "Olá, ",
+                    style: TextStyles.titleRegular,
+                    children: [
+                      TextSpan(
+                        text: "Savio",
+                        style: TextStyles.titleBoldBackground,
+                      ),
+                    ]),
+              ),
+              subtitle: Text(
+                "Mantenha suas contas em dia",
+                style: TextStyles.captionShape,
+              ),
+              trailing: Container(
+                height: 48,
+                width: 48,
+                decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(5)),
+                child: const SizedBox(),
+              ),
+            ),
+          ),
+        ),
+      ),
+      body: pages[homeController.currentPage],
+      bottomNavigationBar: SizedBox(
+        height: 90,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+              onPressed: () {
+                homeController.setPage(0);
+                setState(() {});
+              },
+              icon: const Icon(
+                Icons.home,
+                color: AppColors.primary,
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                print("Clicou na caixa do meio");
+              },
+              child: Container(
+                width: 56,
+                height: 56,
+                decoration: BoxDecoration(
+                    color: AppColors.primary,
+                    borderRadius: BorderRadius.circular(5)),
+                child: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.add_box_outlined,
+                    color: AppColors.background,
+                  ),
+                ),
+              ),
+            ),
+            IconButton(
+              onPressed: () {
+                homeController.setPage(1);
+                setState(() {});
+              },
+              icon: const Icon(
+                Icons.description_outlined,
+                color: AppColors.body,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
