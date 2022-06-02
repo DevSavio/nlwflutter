9e92c38c174d974628fa727096ecea0d2b9b56a2
DevSavio <develo_savio@gmail.com>
2022-06-02 11:25:35 -0300

    Adicionado o controller na tela de login

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nlwflutter/modules/login/login_controller.dart';
 import 'package:nlwflutter/shared/themes/app_images.dart';
 import 'package:nlwflutter/shared/themes/app_text_styles.dart';
 import 'package:nlwflutter/shared/themes/appcolors.dart';
@@ -12,6 +13,7 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final controller = LoginController();
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -63,7 +65,7 @@ class _LoginPageState extends State<LoginPage> {
                     ),
                     child: SocialLoginButton(
                       onTap: () {
-                        print("Login com o Google");
+                        controller.googleSignIn(context);
                       },
                     ),
                   ),
